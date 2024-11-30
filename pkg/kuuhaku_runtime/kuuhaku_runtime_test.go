f5ec6b7b360eddf8dd6f7560b1c58cb1bf5b13ad
Ciii <ciii1@proton.me>
2024-11-30 19:20:47 +0700

    Revert "fix index out of range bug on finding a newline in the function addToPositionFromSlicedString"
    This reverts commit caef5da1a6d968990ed7d048c1220e85416df201.

--- pkg/kuuhaku_runtime/kuuhaku_runtime_test.go
+++ pkg/kuuhaku_runtime/kuuhaku_runtime_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestRuntime1(t *testing.T) {
-	ast, errs := kuuhaku_parser.Parse("SEARCH_MODE E{C D = $0 \" \" $1 \" \"} C{<a>} D{<b>}")
+	ast, errs := kuuhaku_parser.Parse("SEARCH_MODE E{C D = $0 \" \" $1 \" \"} C{<a>} D{<b>}");
 	if len(errs) != 0 {
 		println("Expected parser errors length to be 0")
 		helper.DisplayAllErrors(errs)
@@ -26,7 +26,7 @@ func TestRuntime1(t *testing.T) {
 
 	if len(errs) != 0 {
 		println("Expected runtime errors length to be 0, got " + strconv.Itoa(len(errs)))
-		println(err.Error())
+		println(err.Error());
 		t.Fatal()
 	}
 
@@ -38,7 +38,7 @@ func TestRuntime1(t *testing.T) {
 
 func TestRuntime2(t *testing.T) {
 	println("TestRuntime2:")
-	ast, errs := kuuhaku_parser.Parse("SEARCH_MODE E{C D C = $0 $1 \"\\n\" $2} C{<[A-Za-z0-9]+>} D{<\\.>}")
+	ast, errs := kuuhaku_parser.Parse("SEARCH_MODE E{C D C = $0 $1 \"\\n\" $2} C{<[A-Za-z0-9]+>} D{<\\.>}");
 	if len(errs) != 0 {
 		println("Expected parser errors length to be 0")
 		helper.DisplayAllErrors(errs)
@@ -54,35 +54,7 @@ func TestRuntime2(t *testing.T) {
 
 	if err != nil {
 		println("Expected runtime errors length to be 0, got " + strconv.Itoa(len(errs)))
-		println(err.Error())
-		t.Fatal()
-	}
-
-	if strRes != "test.\nHello test2.\nHello3" {
-		println("Expected the string to be \"test.\nHello test2.\nHello3\", got \"" + strRes + "\"")
-		t.Fatal()
-	}
-}
-
-func TestRuntime3(t *testing.T) {
-	println("TestRuntime3:")
-	ast, errs := kuuhaku_parser.Parse("SEARCH_MODE E{E l nl = $0 $1 $2 \"\\t\"} E{l nl = $0 $1 \"\\t\"} l{<test>} nl{<\\n>}")
-	if len(errs) != 0 {
-		println("Expected parser errors length to be 0")
-		helper.DisplayAllErrors(errs)
-		t.Fatal()
-	}
-	res, errs := kuuhaku_analyzer.Analyze(&ast)
-	if len(errs) != 0 {
-		println("Expected analyzer errors length to be 0, got " + strconv.Itoa(len(errs)))
-		helper.DisplayAllErrors(errs)
-		t.Fatal()
-	}
-	strRes, err := Format("test\ntest\ntest\ntest", &res)
-
-	if err != nil {
-		println("Expected runtime errors length to be 0, got " + strconv.Itoa(len(errs)))
-		println(err.Error())
+		println(err.Error());
 		t.Fatal()
 	}
 
